1e1c21c23bfc48f32d3e33e07fd27dbf924a80fb
annika <annika.carlson3@gmail.com>
2023-10-12 01:19:01 -0400

    Not working yet, but made progress

---
+++ fall2023/controltheory/Carlson_AEEM4042_MidtermQ6.py
@@ -0,0 +1,61 @@
+import sys
+sys.path.append('/Users/annikacarlson/Documents/school/controltheory/library')
+import matplotlib.pyplot as plt
+import library.bbParam as P
+from library.bbForceAnimation import bbForceAnimation
+from library.bbDynamics import bbDynamics
+from library.bbController import bbController
+from library.signalGenerator import signalGenerator
+import keyboard
+
+# instantiate VTOL, controller, and animation
+ballbeam = bbDynamics(alpha=0.0)
+control = bbController()
+reference = signalGenerator(amplitude=0.15, frequency=0.01)
+animation = bbForceAnimation(limits=0.7, multfigs=False)
+
+# add subplots
+# z_plot = animation.fig.add_subplot(2, 2, 2)
+# f_plot = animation.fig.add_subplot(2, 2, 4)
+
+# empty lists for plotting
+sim_times = []
+zs = []
+fs = []
+zrs = []
+
+# set initial values
+z = 0
+u = 0
+zr = 0.25
+
+t = P.t_start  # time starts at t_start
+while t < P.t_end:
+    t_next_plot = t + P.t_plot
+
+    while t < t_next_plot:
+        zr = reference.square(t)
+        F = control.update(zr, ballbeam.state)
+        y = ballbeam.update(F)  # Propagate the dynamics
+
+    animation.update(ballbeam.state)
+
+    # sim_times.append(t)
+    # zs.append(y[1][0])
+    # fs.append(F)
+    # z_targets.append(z_target)
+
+    # z_plot.clear(); f_plot.clear()
+    # z_plot.plot(sim_times, zs, label="state", color='c')
+    # z_plot.plot(sim_times, z_targets, label="target", color='m')
+    # z_plot.legend(loc="upper left")
+    # z_plot.set_ylabel("z (m)")
+    # z_plot.grid()
+
+    # f_plot.plot(sim_times, fs, color = 'c')
+    # f_plot.set_ylabel("Total Force (N)")
+    # f_plot.grid()
+
+    plt.pause(0.01)
+    t += P.Ts
+    if keyboard.is_pressed("q"): break
--- fall2023/controltheory/library/bbController.py
+++ fall2023/controltheory/library/bbController.py
@@ -1,25 +1,44 @@
 import library.bbParam as P
+import numpy as np
 
 class bbController:
     def __init__(self):
         self.m1 = P.m1
         self.m2 = P.m2
+        self.l = P.l
         self.Fmax = P.Fmax
 
-        self.kPtheta = 1.825
-        self.kDtheta = 1.173
-        self.KPz = -0.00494
+        self.kPth = 1.825
+        self.kDth = 1.173
+        self.kPz = -0.00494
         self.kDz = -0.0317
         self.kDC = 1.
         self.t_rz = 10.
-        self.t_rtheta = 1.
+        self.t_rth = 1.
 
-    def update(self, zr, thetar, state):
+    # SPLIT BALL AND BEAM??
+    def update(self, zr, state):
         z = state[0][0]
         zdot = state[1][0]
         theta = state[2][0]
         thetadot = state[3][0]
-        feq = P.k*z
-        fc = self.kP*(zc - z)  - self.kD*zdot
-        f = feq + fc
-        return f[0]
+
+        # Figure out how these work
+        # feq = P.k*z
+        # fc = self.kPz*(zr - z)  - self.kD*zdot
+        # f = feq + fc
+
+        # Think this is all wrong, see pg 119 in the book
+        # fball = (P.m1*P.g*z)/P.l
+        # fc = self.kPz*(zr - z) - self.kDz*zdot
+        # f = fball + fc
+
+        thetar = self.kPz*(zr - z) - self.kDz*zdot
+        F = self.kPth*(thetar - theta) - self.kDth*thetadot
+        F = self.saturate(F, self.Fmax)
+        return F
+    
+    def saturate(self, u, limit):
+        if abs(u) > limit:
+            u = limit*np.sign(u)
+        return u
