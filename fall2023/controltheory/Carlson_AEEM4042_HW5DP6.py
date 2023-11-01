c42cca50487d22aba762175b99e005e868861b72
annika <annika.carlson3@gmail.com>
2023-10-31 21:21:42 -0400

    HW5 QDP6 done

---
+++ fall2023/controltheory/Carlson_AEEM4042_HW5DP6.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import control as ctl
+from control.matlab import *
+import library.smdParam as P
+
+# Create transfer function numerator and denominator
+kD = 7.2
+kP = 3.05
+num = [1]
+den = [P.m, (P.b + kD), (P.k + kP), 0.]
+
+# Run transfer function
+sys = ctl.tf(num, den)
+
+# Create root locus plot
+ctl.rlocus(sys)
+plt.show()
