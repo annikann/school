04fba1f2268fc9bba458dcd5cbf8e57aa59678fc
annika <annika.carlson3@gmail.com>
2023-11-02 18:00:47 -0400

    working on QF10

---
+++ fall2023/controltheory/Carlson_AEEM4042_HW5F10.py
@@ -0,0 +1,98 @@
+import sys
+sys.path.append('/Users/annikacarlson/Documents/school/controltheory/library')
+import matplotlib.pyplot as plt
+import library.vtolParam2 as P
+import numpy as np
+from library.vtolAnimation import vtolAnimation
+from library.vtolDynamics2 import vtolDynamics
+from library.vtolControllerPID import vtolControllerPID
+import keyboard
+
+# instantiate VTOL, controller, and animation
+VTOL = vtolDynamics(alpha=0.0)
+control = vtolControllerPID(10., 0.05, flag=True)
+animation = vtolAnimation(limits=10, multfigs=True)
+
+# Set parameters to tune controller
+kPth = 0.3721
+kDth = 0.19134
+kPz = -0.00771
+kDz = -0.03285
+kPh = 0.1134
+kDh = 0.5833
+kIh = 0.
+kIth = 0.
+kIz = 0.
+
+# add subplots
+z_plot = animation.fig.add_subplot(4, 2, 2)
+h_plot = animation.fig.add_subplot(4, 2, 4)
+theta_plot = animation.fig.add_subplot(4, 2, 6)
+f_plot = animation.fig.add_subplot(4, 2, 8)
+
+# create lists for plotting
+zs = [P.z0]
+thetas = [0.]
+hs = [P.h0]
+frs = [0]
+fls = [0]
+h_target = 5.
+z_target = 0.
+h_targets = [h_target]
+z_targets = [z_target]
+
+t = P.t_start  # time starts at t_start
+sim_times = [t]
+while t < P.t_end:
+
+    t_next_plot = t + P.t_plot
+    while t < t_next_plot:
+        if t > 1.0:
+            h_target = 5.
+            z_target = 0.
+    
+        z, h, theta = VTOL.h().flatten()
+        fr, fl = control.update(h_target, z_target, h, z, theta)
+        y = VTOL.update(fr, fl)
+        t += P.Ts
+
+    sim_times.append(t)
+    zs.append(y[0][0])
+    hs.append(y[1][0])
+    thetas.append(np.rad2deg(y[2][0]))
+    frs.append(fr)
+    fls.append(fl)
+    h_targets.append(h_target)
+    z_targets.append(z_target)
+
+    animation.update(VTOL.state)
+
+    z_plot.clear(); h_plot.clear(); theta_plot.clear(); f_plot.clear()
+
+    z_plot.plot(sim_times, zs, label="state", color='c')
+    z_plot.plot(sim_times, z_targets, label="target", color='m')
+    # z_plot.set_ylim(-1,7)
+    z_plot.legend(loc="upper left")
+    z_plot.set_ylabel("Z (m)")
+    z_plot.grid()
+
+    h_plot.plot(sim_times, hs, label="state", color='c')
+    h_plot.plot(sim_times, h_targets, label="target", color='m')
+    # h_plot.set_ylim(0,10)
+    h_plot.legend(loc="upper left")
+    h_plot.set_ylabel("Height (m)")
+    h_plot.grid()
+
+    theta_plot.plot(sim_times, thetas, color='c')
+    theta_plot.set_ylabel("Theta (deg)")
+    theta_plot.grid()
+
+    f_plot.plot(sim_times, frs, label="right", color = 'c')
+    f_plot.plot(sim_times, fls, label="left", color = 'm')
+    f_plot.legend(loc="upper left")
+    f_plot.set_ylabel("Forces (N)")
+    f_plot.grid()
+
+    plt.pause(0.01)
+
+    if keyboard.is_pressed("q"): break
---
+++ fall2023/controltheory/library/vtolControllerPID.py
@@ -0,0 +1,176 @@
+import library.vtolParam as P
+import numpy as np
+
+class vtolControllerPID:
+    def __init__(self, Fmax, sigma, flag):
+        self.mr = P.mr
+        self.mc = P.mc
+        self.Jc = P.Jc
+        self.mu = P.mu
+        self.d = P.d
+        self.g = P.g
+        self.kPth = 0.3721
+        self.kDth = 0.19134
+        self.kPz = -0.00771
+        self.kDz = -0.03285
+        self.kPh = 0.1134
+        self.kDh = 0.5833
+        self.kih = 0.
+        self.kit = 0.
+        self.kiz = 0.
+        self.limit = Fmax
+        self.sigma = sigma
+        self.flag = flag
+        self.beta = (2.0*sigma-P.Ts)/(2.0*sigma+P.Ts)
+        self.Ts = P.Ts # sample rate
+        self.y_dotz = 0.0 # estimated derivative of y
+        self.y_d1z = 0.0 # Signal y delayed by one sample
+        self.error_dotz = 0.0 # estimated derivative of error
+        self.error_d1z = 0.0 # Error delayed by one sample
+        self.integratorz = 0.0 # integrator
+        self.y_dott = 0.0 # estimated derivative of y
+        self.y_d1t = 0.0 # Signal y delayed by one sample
+        self.error_dott = 0.0 # estimated derivative of error
+        self.error_d1t = 0.0 # Error delayed by one sample
+        self.integratort = 0.0 # integrator
+        self.y_doth = 0.0 # estimated derivative of y
+        self.y_d1h = 0.0 # Signal y delayed by one sample
+        self.error_doth = 0.0 # estimated derivative of error
+        self.error_d1h = 0.0 # Error delayed by one sample
+        self.integratorh = 0.0 # integrator
+    
+    
+    def update(self, hc, zc, h, z, theta):
+        # Theta
+        taueq = 0.
+        zs = self.PIDz(zc, z)
+        tauc = self.PIDt(zs, theta)
+        tau = taueq + tauc
+        
+        # Height
+        feq = self.g*(self.mc + 2*self.mr)
+        fc = self.PIDh(hc, h)
+        f = feq + fc
+        
+        # Forces
+        fr = (tau + self.d*f)/(2*self.d)
+        fl = f - fr
+        
+        # Saturate
+        fr = self.saturate(fr)
+        fl = self.saturate(fl)
+        
+        return fr, fl
+    
+    
+    def PIDz(self, y_r, y):
+        # Compute the current error
+        error = y_r - y
+        # integrate error using trapazoidal rule
+        self.integratorz = self.integratorz \
+        + (self.Ts/2) * (error + self.error_d1z)
+        # PID Control
+        if self.flag is True:
+            # differentiate error
+            self.error_dotz = self.beta * self.error_dotz \
+                + (1-self.beta)/self.Ts * (error - self.error_d1z)
+            # PID control
+            u_unsat = self.kPz*error \
+                + self.kiz*self.integratorz \
+                + self.kDz*self.error_dotz
+        else:
+            # differentiate y
+            self.y_dotz = self.beta * self.y_dotz \
+            + (1-self.beta)/self.Ts * (y - self.y_d1z)
+            # PID control
+            u_unsat = self.kPz*error \
+                + self.kiz*self.integratorz \
+                - self.kDz*self.y_dotz
+        # return saturated control signal
+        u_sat = self.saturate(u_unsat)
+        # integrator anti - windup
+        if self.kiz != 0.0:
+            self.integratorz = self.integratorz \
+                + 1.0 / self.kiz * (u_sat - u_unsat)
+        # update delayed variables
+        self.error_d1z = error
+        self.y_d1z = y
+        return u_sat
+    
+    
+    def PIDt(self, y_r, y):
+        # Compute the current error
+        error = y_r - y
+        # integrate error using trapazoidal rule
+        self.integratort = self.integratort \
+        + (self.Ts/2) * (error + self.error_d1t)
+        # PID Control
+        if self.flag is True:
+            # differentiate error
+            self.error_dott = self.beta * self.error_dott \
+                + (1-self.beta)/self.Ts * (error - self.error_d1t)
+            # PID control
+            u_unsat = self.kPth*error \
+                + self.kit*self.integratort \
+                + self.kDth*self.error_dott
+        else:
+            # differentiate y
+            self.y_dott = self.beta * self.y_dott \
+            + (1-self.beta)/self.Ts * (y - self.y_d1t)
+            # PID control
+            u_unsat = self.kPth*error \
+                + self.kit*self.integratort \
+                - self.kDth*self.y_dott
+        # return saturated control signal
+        u_sat = self.saturate(u_unsat)
+        # integrator anti - windup
+        if self.kit != 0.0:
+            self.integratort = self.integratort \
+                + 1.0 / self.kit * (u_sat - u_unsat)
+        # update delayed variables
+        self.error_d1t = error
+        self.y_d1t = y
+        return u_sat
+    
+    
+    def PIDh(self, y_r, y):
+        # Compute the current error
+        error = y_r - y
+        # integrate error using trapazoidal rule
+        self.integratorh = self.integratorh \
+        + (self.Ts/2) * (error + self.error_d1h)
+        # PID Control
+        if self.flag is True:
+            # differentiate error
+            self.error_doth = self.beta * self.error_doth \
+                + (1-self.beta)/self.Ts * (error - self.error_d1h)
+            # PID control
+            u_unsat = self.kPh*error \
+                + self.kih*self.integratorh \
+                + self.kDh*self.error_doth
+        else:
+            # differentiate y
+            self.y_doth = self.beta * self.y_doth \
+            + (1-self.beta)/self.Ts * (y - self.y_d1h)
+            # PID control
+            u_unsat = self.kPh*error \
+                + self.kih*self.integratorh \
+                - self.kDh*self.y_doth
+        # return saturated control signal
+        u_sat = self.saturate(u_unsat)
+        # integrator anti - windup
+        if self.kih != 0.0:
+            self.integratorh = self.integratorh \
+                + 1.0 / self.kih * (u_sat - u_unsat)
+        # update delayed variables
+        self.error_d1h = error
+        self.y_d1h = y
+        return u_sat
+    
+    
+    def saturate(self,u):
+        if u > self.limit:
+            u = self.limit
+        elif u < 0:
+            u = 0
+        return u
