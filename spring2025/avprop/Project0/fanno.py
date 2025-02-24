b5f45913a1419364d1d04bba91ddf68049a796b7
annika <annika.carlson3@gmail.com>
2025-02-24 00:46:50 -0500

    reformatting (still a WIP)

--- spring2025/avprop/Project0/fanno.py
+++ spring2025/avprop/Project0/fanno.py
@@ -6,52 +6,60 @@ import numpy as np
 from scipy.optimize import fsolve
 from tabulate import tabulate
 
-def I_Istar_Fanno(M, gamma):
-    I_Istar = (1 + gamma*(M**2))/(M*np.sqrt(2*(gamma + 1)*(1 + ((gamma - 1)/2)*(M**2))))
-    return I_Istar
+def FannoFlow(M1:float, cf:float, L:float, D:float, y:float):
+    """
+    Function to calculate change in flow parameters with friction (Fanno Flow).
+    
+    Parameters
+    ----------
+    M1 : float
+        Mach number before heat addition.
+    cf : float
+        Coefficient of friction.
+    L : float
+        Flow length.
+    D : float
+        Flow diameter.
+    y : float
+        Ratio of specific heats.
 
-def T_Tstar_Fanno(M, gamma):
-    T_Tstar = (gamma + 1)/(2 + (gamma - 1)*(M**2))
-    return T_Tstar
-
-def P_Pstar_Fanno(M, gamma):
-    P_Pstar = (1/M)*np.sqrt((gamma + 1)/(2 + (gamma - 1)*(M**2)))
-    return P_Pstar
-
-def rho_rhostar_Fanno(M, gamma):
-    rho_rhostar = (1/M)*np.sqrt((2 + (gamma - 1)*(M**2))/(gamma + 1))
-    return rho_rhostar
-
-def Po_Postar_Fanno(M, gamma):
-    Po_Postar = (1/M)*((2 + (gamma - 1)*(M**2))/(gamma + 1))**((gamma + 1)/(2*(gamma - 1)))
-    return Po_Postar
-
-def FannoFlow(M1, cf, L, D, gamma):
-
+    Returns
+    -------
+    Ms[i] : float
+        Mach number.
+    P_Pstar : float
+        Sonic reference condition static pressure ratio.
+    T_Tstar : float
+        Sonic reference condition static temperature ratio.
+    Po_Postar : float
+        Sonic reference condition total pressure ratio.
+    rho_rhostar : float
+        Sonic reference condition density ratio.
+    """
     fL_D_act = (4*cf*L)/D
-    fLmax_D_1 = ((1 - M1**2)/(gamma*(M1**2))) + ((gamma + 1)/(2*gamma))*np.log(((gamma + 1)*M1**2)/(2 + (gamma - 1)*M1**2))
+    fLmax_D_1 = ((1 - M1**2)/(y*(M1**2))) + ((y + 1)/(2*y))*np.log(((y + 1)*M1**2)/(2 + (y - 1)*M1**2))
     fLmax_D_2 = fLmax_D_1 - fL_D_act
 
-    def equation(M2, fLmax_D_2, gamma):
-        return ((1 - M2**2)/(gamma*(M2**2))) + ((gamma + 1)/(2*gamma))*np.log(((gamma + 1)*M2**2)/(2 + (gamma - 1)*M2**2)) - fLmax_D_2
+    def equation(M2, fLmax_D_2, y):
+        return ((1 - M2**2)/(y*(M2**2))) + ((y + 1)/(2*y))*np.log(((y + 1)*M2**2)/(2 + (y - 1)*M2**2)) - fLmax_D_2
 
-    def solve_M(fLmax_D_2, gamma):
+    def solve_M(fLmax_D_2, y):
         M_subsonic_guess = 0.1
         M_supersonic_guess = 1.01
-        M_subsonic = fsolve(equation, M_subsonic_guess, args=(fLmax_D_2, gamma))[0]
-        M_supersonic = fsolve(equation, M_supersonic_guess, args=(fLmax_D_2, gamma))[0]
+        M_subsonic = fsolve(equation, M_subsonic_guess, args=(fLmax_D_2, y))[0]
+        M_supersonic = fsolve(equation, M_supersonic_guess, args=(fLmax_D_2, y))[0]
         return np.array([M_subsonic, M_supersonic])
     
-    M2 = solve_M(fLmax_D_2, gamma)[0] if M1 < 1.0 else solve_M(fLmax_D_2, gamma)[1]
+    M2 = solve_M(fLmax_D_2, y)[0] if M1 < 1.0 else solve_M(fLmax_D_2, y)[1]
     
     Ms = [M1, M2]
     results = []
     for i in range(len(Ms)):
-        I_Istar = I_Istar_Fanno(Ms[i], gamma)
-        P_Pstar = P_Pstar_Fanno(Ms[i], gamma)
-        T_Tstar = T_Tstar_Fanno(Ms[i], gamma)
-        Po_Postar = Po_Postar_Fanno(Ms[i], gamma)
-        rho_rhostar = rho_rhostar_Fanno(Ms[i], gamma)
+        I_Istar = (1 + y*(Ms[i]**2))/(Ms[i]*np.sqrt(2*(y + 1)*(1 + ((y - 1)/2)*(Ms[i]**2))))
+        P_Pstar = (1/Ms[i])*np.sqrt((y + 1)/(2 + (y - 1)*(Ms[i]**2)))
+        T_Tstar = (y + 1)/(2 + (y - 1)*(Ms[i]**2))
+        Po_Postar = (1/Ms[i])*((2 + (y - 1)*(Ms[i]**2))/(y + 1))**((y + 1)/(2*(y - 1)))
+        rho_rhostar = (1/Ms[i])*np.sqrt((2 + (y - 1)*(Ms[i]**2))/(y + 1))
 
         if i == 0: fLmax_D = fLmax_D_1
         else: fLmax_D = fLmax_D_2
@@ -64,7 +72,7 @@ def FannoFlow(M1, cf, L, D, gamma):
             ["P / P*", f"{P_Pstar:.4f}"],
             ["Po / Po*", f"{Po_Postar:.4f}"],
             ["T / T*", f"{T_Tstar:.4f}"],
-            #["ρ / ρ*", f"{rho_rhostar:.4f}"],
+            ["ρ / ρ*", f"{rho_rhostar:.4f}"],
         ]
 
         print("\nFanno Flow Results")
--- spring2025/avprop/Project0/rayleigh.py
+++ spring2025/avprop/Project0/rayleigh.py
@@ -6,68 +6,69 @@ import numpy as np
 from scipy.optimize import fsolve
 from tabulate import tabulate
 
-def phi_Msqd_Rayleigh(M, gamma):
-    phi_Msqd = ((M**2)*(1 + ((gamma - 1)/2)*(M**2)))/((1 + gamma*(M**2))**2)
-    return phi_Msqd
+def Mach_Rayleigh(M1:float, y:float, To_Tostar:float):
+    """
+    Function to calculate mach number after heat addition.
+    """
+    def equation(M, To_Tostar, y):
+        return ((((y + 1) * (M**2)) / ((1 + y * (M**2))**2)) * (2 + (y - 1) * (M**2))) - To_Tostar
 
-def P_Pstar_Rayleigh(M, gamma):
-    P_Pstar = (1 + gamma) / (1 + gamma*M**2)
-    return P_Pstar
-
-def T_Tstar_Rayleigh(M, gamma):
-    T_Tstar = ((M**2)*(1 + gamma)**2) / ((1 + gamma*(M**2))**2)
-    return T_Tstar
-
-def rho_rhostar_Rayleigh(M, gamma):
-    rho_rhostar = (1/(M**2))*((1 + gamma*(M**2))/(1 + gamma))
-    return rho_rhostar
-
-def Po_Postar_Rayleigh(M, gamma):
-    P_Pstar = P_Pstar_Rayleigh(M, gamma)
-    Po_Postar = P_Pstar*((1+(M**2)*(gamma - 1)/2)**(gamma/(gamma - 1)))/(1 + (gamma - 1)/2)**(gamma/(gamma - 1))
-    return Po_Postar
-
-def To_Tostar_Rayleigh(M, gamma):
-    To_Tostar = ((M**2)*(1 + gamma)**2 / (1 + gamma*(M**2))**2) * (1 + (M**2)*(gamma - 1)/2)/(1 + (gamma - 1)/2)
-    return To_Tostar 
-
-def V_Vstar_Rayleigh(M, gamma):
-    P_Pstar = P_Pstar_Rayleigh(M, gamma)
-    T_Tstar = T_Tstar_Rayleigh(M, gamma)
-    V_Vstar = T_Tstar/P_Pstar 
-    return V_Vstar
-
-def Mach_Rayleigh(M1, gamma, To_Tostar):
-
-    def equation(M, To_Tostar, gamma):
-        return ((((gamma + 1) * (M**2)) / ((1 + gamma * (M**2))**2)) * (2 + (gamma - 1) * (M**2))) - To_Tostar
-
-    def solve_M(To_Tostar, gamma):
+    def solve_M(To_Tostar, y):
         M_subsonic_guess = 0.1
         M_supersonic_guess = 1.01
-        M_subsonic = fsolve(equation, M_subsonic_guess, args=(To_Tostar, gamma))[0]
-        M_supersonic = fsolve(equation, M_supersonic_guess, args=(To_Tostar, gamma))[0]
+        M_subsonic = fsolve(equation, M_subsonic_guess, args=(To_Tostar, y))[0]
+        M_supersonic = fsolve(equation, M_supersonic_guess, args=(To_Tostar, y))[0]
         return np.array([M_subsonic, M_supersonic])
     
-    return solve_M(To_Tostar, gamma)[0] if M1 < 1.0 else solve_M(To_Tostar, gamma)[1]
+    return solve_M(To_Tostar, y)[0] if M1 < 1.0 else solve_M(To_Tostar, y)[1]
 
-def RayleighFlow(M1, gamma, To_Tostar_2):
+def RayleighFlow(M1, y, To_Tostar_2):
+    """
+    Function to calculate change in flow parameters with heat addition (Rayleigh Flow).
+    
+    Parameters
+    ----------
+    M1 : float
+        Mach number before heat addition.
+    y : float
+        Specific heat ratio.
+    To_Tostar_2 : float
+        Sonic reference condition total temperature ratio, after heat addition.
 
+    Returns
+    -------
+    M : float
+        Mach number.
+    phi_Msqd : float
+        ADD DESCRIPTION.
+    P_Pstar : float
+        Sonic reference condition static pressure ratio.
+    T_Tstar : float
+        Sonic reference condition static temperature ratio.
+    rho_rhostar : float
+        Sonic reference condition density ratio.
+    Po_Postar : float
+        Sonic reference condition total pressure ratio.
+    To_Tostar: float
+        Sonic reference condition total pressure ratio.
+    V_Vstar : float
+        Sonic reference condition velocity ratio.
+    """
     # Compute Mach number or To_Tostar ratio based on inputs
     if To_Tostar_2 is None:
         M = M1
-        To_Tostar = To_Tostar_Rayleigh(M, gamma)
+        To_Tostar = ((M**2)*(1 + y)**2 / (1 + y*(M**2))**2) * (1 + (M**2)*(y - 1)/2)/(1 + (y - 1)/2)
     else:
-        M = Mach_Rayleigh(M1, gamma, To_Tostar_2)
+        M = Mach_Rayleigh(M1, y, To_Tostar_2)
         To_Tostar = To_Tostar_2
 
     # Compute all ratios
-    phi_Msqd = phi_Msqd_Rayleigh(M, gamma)
-    P_Pstar = P_Pstar_Rayleigh(M, gamma)
-    T_Tstar = T_Tstar_Rayleigh(M, gamma)
-    rho_rhostar = rho_rhostar_Rayleigh(M, gamma)
-    Po_Postar = Po_Postar_Rayleigh(M, gamma)
-    V_Vstar = V_Vstar_Rayleigh(M, gamma)
+    phi_Msqd = ((M**2)*(1 + ((y - 1)/2)*(M**2)))/((1 + y*(M**2))**2)
+    P_Pstar = (1 + y)/(1 + y*M**2)
+    T_Tstar = ((M**2)*(1 + y)**2) / ((1 + y*(M**2))**2)
+    rho_rhostar = (1/(M**2))*((1 + y*(M**2))/(1 + y))
+    Po_Postar = P_Pstar*((1+(M**2)*(y - 1)/2)**(y/(y - 1)))/(1 + (y - 1)/2)**(y/(y - 1))
+    V_Vstar = T_Tstar/P_Pstar 
 
     # Create a table with labeled values
     headers = ["Parameter", "Value"]
@@ -75,7 +76,7 @@ def RayleighFlow(M1, gamma, To_Tostar_2):
         ["Mach Number (M)", f"{M:.4f}"],
         ["𝜙(M^2)", f"{phi_Msqd:.4f}"],
         ["P / P*", f"{P_Pstar:.4f}"],
-        #["ρ / ρ*", f"{rho_rhostar:.4f}"],
+        ["ρ / ρ*", f"{rho_rhostar:.4f}"],
         ["Po / Po*", f"{Po_Postar:.4f}"],
         ["T / T*", f"{T_Tstar:.4f}"],
         ["To / To*", f"{To_Tostar:.4f}"],
@@ -85,9 +86,7 @@ def RayleighFlow(M1, gamma, To_Tostar_2):
     print("\nRayleigh Flow Results")
     print(tabulate(table, headers, tablefmt="grid", floatfmt=".4f"))
 
-    results = [M, P_Pstar, T_Tstar, rho_rhostar, Po_Postar, To_Tostar, V_Vstar]
-
-    return results
+    return [M, phi_Msqd, P_Pstar, T_Tstar, rho_rhostar, Po_Postar, To_Tostar, V_Vstar]
 
 
 
